0bef0fdecccdcebf43a8014999cd7caede914a6b
Sena Zeleke <senazeleke258@gmail.com>
2026-09-18 14:00:50 +0300

    Store social profile URLs in one constants file.

---
+++ src/constants/social.js
@@ -0,0 +1,6 @@
+export const SOCIAL_LINKS = [
+  { id: 1, label: 'LinkedIn', href: 'https://linkedin.com', icon: 'linkedin' },
+  { id: 2, label: 'Github', href: 'https://github.com/senazeleke', icon: 'github' },
+  { id: 3, label: 'Mail', href: 'mailto:senazeleke258@gmail.com', icon: 'mail' },
+  { id: 4, label: 'Telegram', href: 'https://t.me/Theotokos_ICU', icon: 'telegram' },
+];
