be41e9abf7d8523f7aa0f912ad9f0951a826d62f
Sena Zeleke <senazeleke258@gmail.com>
2026-09-18 14:00:50 +0300

    Add email and Telegram contact links.

---
+++ src/constants/contact.js
@@ -0,0 +1,6 @@
+export const CONTACT = {
+  email: 'senazeleke258@gmail.com',
+  emailHref: 'mailto:senazeleke258@gmail.com',
+  telegramHref: 'https://t.me/Theotokos_ICU',
+  telegramLabel: 'Telegram',
+};
