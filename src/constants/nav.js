3d8918af71dc616093578d8e425fb383ae12d638
Sena Zeleke <senazeleke258@gmail.com>
2026-09-18 14:00:49 +0300

    Extract navbar links and scroll timing.

---
+++ src/constants/nav.js
@@ -0,0 +1,14 @@
+import { SECTION_IDS } from './routes';
+
+export const NAV_LINKS = [
+  { id: 1, link: SECTION_IDS.home },
+  { id: 2, link: SECTION_IDS.about },
+  { id: 3, link: SECTION_IDS.skills },
+  { id: 4, link: SECTION_IDS.projects },
+  { id: 5, link: SECTION_IDS.contact },
+];
+
+export const SCROLL = {
+  smooth: true,
+  duration: 800,
+};
