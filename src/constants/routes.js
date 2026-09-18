a27fab1bfa8d617d451ce8a738d992f1bc9f528d
Sena Zeleke <senazeleke258@gmail.com>
2026-09-18 14:00:48 +0300

    Centralize app routes and section ids.

---
+++ src/constants/routes.js
@@ -0,0 +1,12 @@
+export const ROUTES = {
+  home: '/',
+  ecommerce: '/ecommerce',
+};
+
+export const SECTION_IDS = {
+  home: 'home',
+  about: 'about',
+  skills: 'skills',
+  projects: 'projects',
+  contact: 'contact',
+};
