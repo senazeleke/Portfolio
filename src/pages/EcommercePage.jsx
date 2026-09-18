329861329c8276041e189d4c6602b29ce2bf5901
Sena Zeleke <senazeleke258@gmail.com>
2026-09-18 14:01:00 +0300

    Add a page wrapper for the eCommerce case study.

---
+++ src/pages/EcommercePage.jsx
@@ -0,0 +1,9 @@
+import Ecommerce from '../components/projects/Ecommerce';
+
+const EcommercePage = () => (
+  <div className="relative z-10">
+    <Ecommerce />
+  </div>
+);
+
+export default EcommercePage;
