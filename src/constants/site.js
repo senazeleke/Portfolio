85e7af789f57ece55a64e98cc302c73223f49a1f
Sena Zeleke <senazeleke258@gmail.com>
2026-09-18 14:00:49 +0300

    Move site name, logo, and CV link into constants.

---
+++ src/constants/site.js
@@ -0,0 +1,11 @@
+import brandLogo from '../assets/logo/sena.png';
+
+export const SITE = {
+  brandName: 'Sena',
+  logo: brandLogo,
+  fullName: 'Sena Zeleke',
+  documentTitle: "Sena's Portfolio",
+  role: 'Software Engineer',
+  tagline: 'I build software and develop full-stack web and mobile applications.',
+  cvUrl: 'https://drive.google.com/file/d/1RRObGLOadWYJ02mb2mbakJ6iRJ0RABYw/view?usp=sharing',
+};
